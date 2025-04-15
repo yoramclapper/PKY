2914d26b62e61690bb449a90688706d33e9083af
yoramclapper <yoram.t.c@gmail.com>
2025-04-15 07:00:49 +0200

    Initial setup to start with url-view patterns

--- pky/budget/urls.py
+++ pky/budget/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import ActualsView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", ActualsView.as_view(), name="index"),
 ]
--- pky/budget/views.py
+++ pky/budget/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class ActualsView(TemplateView):
+    template_name = "budget/actuals.html"
